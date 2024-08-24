01da73458402ca922c6df3bf3804ed062c2080fa
MDixey17 <matthew_dixey@yahoo.com>
2024-08-24 16:09:21 -0500

    404 and other bugs

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,7 +1,7 @@
 import { Service } from "../models/API/Service"
 
 const F1Insights: Service = {
-  // baseUrl: "http://localhost:5125/api",
+  //baseUrl: "http://localhost:5125/api",
   baseUrl: "https://f1-insights-api.azurewebsites.net/api",
 }
 

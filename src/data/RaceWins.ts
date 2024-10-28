6651a81fea176ca2fc2774d4b42b3e4f4ad037a8
MDixey17 <76823860+MDixey17@users.noreply.github.com>
2024-10-28 12:18:18 -0500

    US and Mexico Grand Prix (#13)

--- src/data/RaceWins.ts
+++ src/data/RaceWins.ts
@@ -3,9 +3,9 @@ import { CardStat } from "../models/Data/CardStat"
 export const RaceWins: CardStat = [
   ["Max Verstappen", 7],
   ["Lando Norris", 3],
+  ["Charles Leclerc", 3],
   ["Lewis Hamilton", 2],
-  ["Charles Leclerc", 2],
   ["Oscar Piastri", 2],
-  ["Carlos Sainz", 1],
+  ["Carlos Sainz", 2],
   ["George Russell", 1],
 ]

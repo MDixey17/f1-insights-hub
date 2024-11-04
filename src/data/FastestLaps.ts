7404027ad9bab517f74b0d5bd92c93d6e89dadbc
MDixey17 <76823860+MDixey17@users.noreply.github.com>
2024-11-03 21:34:45 -0600

    Brazil Sprint and GP 2024 (#14)

--- src/data/FastestLaps.ts
+++ src/data/FastestLaps.ts
@@ -3,7 +3,7 @@ import { CardStat } from "../models/Data/CardStat"
 export const FastestLaps: CardStat = [
   ["Lando Norris", 4],
   ["Charles Leclerc", 3],
-  ["Max Verstappen", 2],
+  ["Max Verstappen", 3],
   ["Fernando Alonso", 2],
   ["Lewis Hamilton", 2],
   ["George Russell", 2],
